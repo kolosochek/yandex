70344588823383ee722ea0439ff70fe35e092d29
D <kolosochek@gmail.com>
2022-11-07 17:38:40 +0300

    minor netlify fix

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-const express = require('express')
-const hbs = require('handlebars')
-const app = express()
+const express = require('express');
 const path = require('path');
 const port = 3000
 
 //static 
 const static_url = path.join(__dirname, 'static')
+
+const app = express();
 app.use(express.static(static_url))
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '/index.html'))
-})
 
 app.listen(port, () => {
   console.log(`Express server has been started on port ${port}`)
